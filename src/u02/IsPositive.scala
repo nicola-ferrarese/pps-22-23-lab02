8798aac2c73517d55ff7d98a1ec8cd39a58164cf
nicola.ferrarese <f.nicola98@gmail.com>
2023-03-17 09:29:08 +0100

    * refactor

--- src/u02/IsPositive.scala
+++ src/u02/IsPositive.scala
@@ -15,9 +15,9 @@ object IsPositive extends App:
     case n if n < 0 => "negative"
     case _ => "zero"
   println("Testing Positive func:")
-  println("positive(3): " +positive(3))
-  println("positive(-3): " +positive(-3))
-  println("positive(0): " +positive(0))
+  println("\tpositive(3): " +positive(3))
+  println("\tpositive(-3): " +positive(-3))
+  println("\tpositive(0): " +positive(0))
 
 
   val empty: String => Boolean = _ == "" // predicate on strings
@@ -31,33 +31,34 @@ object IsPositive extends App:
 
   val notEmpty = neg(empty)
   println("Testing neg func:")
-  println("notEmpty(''): " + notEmpty(""))
+  println("\tnotEmpty(''): " + notEmpty(""))
   val notEmpty2 = neg2(empty)
-  println(notEmpty2(""))
+  println("\tnotEmpty2(''): " + notEmpty2(""))
 
   // 4 - currying
-  val curriedFuncType: Int => Int => Int => Boolean =
+  println("Testing currying func: ")
+  val curriedFuncVal: Int => Int => Int => Boolean =
     x => y => z => x <= y && y == z
 
-  println(curriedFuncType(1)(2)(3))
+  println("\tcurriedFuncVal(1)(2)(3): " + curriedFuncVal(1)(2)(3))
 
-  val nonCurriedFuncType: (Int, Int, Int) => Boolean =
+  val nonCurriedFuncVal: (Int, Int, Int) => Boolean =
     (x, y, z) => x <= y && y == z
-  println(nonCurriedFuncType(1, 2, 3))
+  println("\tnonCurriedFuncVal(1,2,2): "+ nonCurriedFuncVal(1, 2, 2))
   def curriedFunc(x: Int)(y: Int)(z: Int): Boolean =
     x <= y && y == z
-  println(curriedFunc(1)(2)(2))
+  println("\tcurriedFunc(1)(2)(2): " + curriedFunc(1)(2)(2))
 
   def nonCurriedFunc(x: Int, y: Int, z: Int): Boolean =
     x <= y && y == z
-  println(nonCurriedFunc(1, 2, 2))
+  println("\tnonCurriedFunc(1,2,3): "+ nonCurriedFunc(1, 2, 3))
+
 
   // 5 - Functional Composition
-  //  Create a function that implements functional compositions:
+  println("Testing compose func:")
   def compose(f: Int => Int, g: Int => Int): Int => Int =
     x => f(g(x))
-
-  println(compose(_ - 1, _ * 2)(3)) // 5
+  println("\tcompose(_ - 1, _ * 2)(3): "+ compose(_ - 1, _ * 2)(3)) // 5
 
   // Generic version of compose
   def compose2[A, B, C](f: B => C, g: A => B): A => C = x => f(g(x))
@@ -65,12 +66,13 @@ object IsPositive extends App:
   // println(compose2(_ - 1, _ * 2)(3)) // 5
 
   // 6 - recursion
+  println("Testing recursion func:")
   @annotation.tailrec
   private def gcd(a: Int, b: Int): Int = b match
     case 0 => a
     case _ => gcd(b, a % b)
 
-  println(gcd(14, 21))
+  println("\tgcd(14, 21): "+gcd(14, 21)) // 7
 
   // 7-
   enum Shape:
@@ -113,7 +115,7 @@ object IsPositive extends App:
   println("Circle:")
   println("\tPerimeter:\n\tExpected: 19.73 - Actual: "+perimeter(c)) // 19.73
   println("\tContains:\n\tExpected: true - Actual: "+contains(c, (1, 1))) // true
-  println("\tExpected: false - Actual: "+contains(c, (-1, -1))) // false
+  println("\tExpected: true - Actual: "+contains(c, (-1, -1))) // false
 
   // 8 - Functional combinators
   enum Option[A]:
@@ -124,36 +126,33 @@ object IsPositive extends App:
     def isEmpty[A](opt: Option[A]): Boolean = opt match
       case None() => true
       case _ =>  false
+
     def orElse[A, B >: A](opt: Option[A], orElse: B): B = opt match
       case Some(a) => a
       case _ => orElse
+
     def flatMap[A, B](opt: Option[A])(f: A => Option[B]): Option[B] = opt match
       case Some(a) => f(a)
       case _ => None()
 
-    //Extend Option[A] with a filter method
-    // filter(Some(5))(_ > 2) = Some(5)
     def filter[A](opt: Option[A])(f: A => Boolean): Option[A] = opt match
       case Some(a) => opt
       case _ => None()
 
-    //function that transform the value within the Option (if
-    //present)
     def map[A, B](opt: Option[A])(f: A => B): Option[B] = opt match
       case Some(a) => Some(f(a))
       case _ => None()
 
-    //Extend Option[A] with a fold method
-    // fold(Some(5))(1)(_ + 1) = 6
     def fold[A,B](opt: Option[A])(z: B)(f: (A) => B): B = opt match
       case Some(a) => f(a)
       case _ => z
 
   import Option.*
-  println(filter(Some(5))(_ > 2)) // Some(5)
-  println(map(Some(5))(_ > 2)) // Some(true)
-  println(map(Some(5))(_ > 8)) // Some(true)
-  println(map(None[Int]())(_ > 2)) // Some(true)
 
-  println(fold(Some(5))(1)( _  + 1))
-  println(fold(None[Int]())(1)(_ + 1))
+  println("Testing Option func:")
+  println("\tfilter(Some(5))(_ > 2): "+filter(Some(5))(_ > 2)) // Some(5)
+  println("\tmap(Some(5))(_ > 2): "+map(Some(5))(_ > 2)) // Some(true)
+  println("\tmap(Some(5))(_ > 8): "+map(Some(5))(_ > 8)) // Some(true)
+  println("\tmap(None[Int]())(_ > 2): "+map(None[Int]())(_ > 2)) // Some(true)
+  println("\tfold(Some(5))(1)( _  + 1): "+fold(Some(5))(1)( _  + 1))
+  println("\tfold(None[Int]())(1)(_ + 1): "+fold(None[Int]())(1)(_ + 1))
